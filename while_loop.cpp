07cffe87da03f6954ed35206393d024f1f82f7ca
Patcharapa05 <aaon3322@gmail.com>
2025-07-29 21:43:51 +0700

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  	// while loop to print numbers from 1 to 5
+    int i = 1;
+    while (i <= 5) {
+        cout << "Hi  => i = " << i << endl;
+        i++; // i = i + 1
+    }
+
+    return 0;
+}
